bf6d9276ef9a2e8f1469180fcfda6aa98f018fc8
peter nagy <peter951nagy@email.com>
2024-04-18 00:59:08 +0200

    using useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,87 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup/ListGroup";
-import { FaRegCalendarPlus } from "react-icons/fa";
-import Like from "./components/Like";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form/Form";
-import FormUseState from "./components/Form/FormUseState";
-import FormRectHook from "./components/Form/FormRectHook";
-import ExpenseTracker from "./components/expense-tracker/components/ExpenseTracker";
-import ExpensesFilter from "./components/expense-tracker/components/ExpensesFilter";
-import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+type User = {
+  id: number;
+  name: string;
+};
 function App() {
-  // // Exercise No.1 Object
-  // const [game, setGame] = useState({
-  //   id: 1,
-  //   player: {
-  //     name: "peter",
-  //   },
-  // });
-  // const handleClick = () => {
-  //   setGame({ ...game, player: { ...game.player, name: "nagy" } });
-  // };
+  const [error, setError] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
 
-  // // Exercise No.2 Array
-  // const [pizza, setPizza] = useState({
-  //   name: "Spicy Pepproni",
-  //   toppings: ["Mushroom"],
-  // });
-  // const handleClick = () => {
-  //   setPizza({ ...pizza, toppings: [...pizza.toppings, "new Sttuf"] });
-  //   console.log(pizza);
-  // };
+  useEffect(() => {
+    // cancelling an http request
+    const controller = new AbortController();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then(({ data: usersData }) => setUsers(usersData))
+      .catch((err) => setError(err.message));
 
-  // // Exercise No.3 Cart
-  // const [cart, setCart] = useState({
-  //   discount: 0.1,
-  //   items: [
-  //     { id: 1, name: "propduct 1", quantity: 1 },
-  //     { id: 2, name: "propduct 2", quantity: 1 },
-  //   ],
-  // });
+    return () => controller.abort();
+  }, []);
 
-  // //
-  // const handleClick = () => {
-  //   setCart({
-  //     ...cart,
-  //     items: cart.items.map((item) =>
-  //       item.id === 1 ? { ...item, quantity: 2 } : item
-  //     ),
-  //   });
-  // };
+  const originalUsers = [...users];
 
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: "Milk",
-      category: "Groceries",
-      amount: 5,
-    },
-    {
-      id: 2,
-      description: "Egg",
-      category: "Utilitis",
-      amount: 5,
-    },
-    {
-      id: 3,
-      description: "chiken",
-      category: "Groceries",
-      amount: 5,
-    },
-  ]);
-  const [selectedCategory, setSelectedCategory] = useState("");
+  let isLoading = false;
+  const addUser = () => {
+    const newUser: User = { id: 0, name: "Mosh" };
+    // setUsers([newUser, ...users]);
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
 
-  const filteredExpenses = selectedCategory
-    ? expenses.filter((e) => e.category == selectedCategory)
-    : expenses;
+  const updateUser = (user: any) => {
+    const updatedUser = { ...user, name: user.name + "!!" };
+    // setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    axios
+      .patch(
+        `https://jsonplaceholder.typicode.com/users/${user.id}`,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        ></ExpenseForm>
-      </div>
+      {error && <p className="text-danger">{error} </p>}
+      {isLoading && <div className="spinner-border"> </div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
 
-      <ExpensesFilter
-        onSelectCategory={(category) => setSelectedCategory(category)}
-      ></ExpensesFilter>
-      <ExpenseTracker
-        expenses={filteredExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      >
-        {" "}
-      </ExpenseTracker>
-      {/* <FormRectHook></FormRectHook> */}
-      {/* <FormUseState></FormUseState> */}
-      {/* <Form></Form> */}
+      <ul className="list-group">
+        {users.map((user: User) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+
+            <div>
+              <button
+                className="mx-1 btn btn-outline-danger"
+                onClick={() => console.log()}
+              >
+                delete
+              </button>
+              <button
+                className="btn btn-outline-secondary"
+                onClick={() => updateUser(user)}
+              >
+                update
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
